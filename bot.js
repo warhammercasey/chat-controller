397f92461780782e207e2b75fa3e8b04fa317698
warhammercasey <warhammercasey@gmail.com>
2018-04-23 17:07:05 -0600

    t

--- bot.js
+++ bot.js
@@ -4,6 +4,35 @@ const client = new Discord.Client();
 const config = require('./config');
 client.login(process.env.BOT_TOKEN);
 
-client.on('message', message => {
+var firstMessage = true;
+var defaultChannels = [[]];
 
+var channels = [[]];
+var guild;
+
+client.on('ready', () => {
+    console.log('Ready');
+    defaultChannels = config.channels.slice();
+});
+
+client.on('message', message => {
+    if (firstMessage) {
+        firstMessage = false;
+        guild = message.guild;
+        console.log('defaultChannels.length =' + defaultChannels.length);
+        for (i = 0; i < defaultChannels.length; i++){
+            for (a = 0; a < defaultChannels[i].length; a++) {
+                channels[i][a] = guild.channels.find("name", defaultChannels[i][a]);
+            }
+        }
+    }
+});
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+    if (oldMember.voiceChannel == null && newMember.voiceChannel != null) {
+        
+    } else if (oldMember.voiceChannel != null && newMember.voiceChannel == null) {
+        
+    } else {
+
+    }
 });
