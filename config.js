8af97db4b542bd99a717d85a7b8dbde487db2c23
warhammercasey <warhammercasey@gmail.com>
2018-04-23 17:09:11 -0600

    t

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 var config = {};
 
-config.categories = ['strike chats', 'raid chats', 'crucible chats'];
 config.channels = [['Strike 1', 'Strike 2', 'Strike 3'], ['Raid 1', 'Raid 2', 'Raid 3'], ['Crucible 1', 'Crucible 2', 'Crucible 3']];
 
 module.exports = config;
