e8defbd710d21c524ba67f9d1df59698fec1e57d
warhammercasey <warhammercasey@gmail.com>
2018-04-28 20:20:52 -0600

    t

--- bot.js
+++ bot.js
@@ -36,7 +36,8 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
     if (oldMember.voiceChannel == null && newMember.voiceChannel != null) {
         categoryChannels = newMember.voiceChannel.parent.children.array();
         var emptyChannels = [];
-        for (i = 0; i < newMember.voiceChannel.parent.children.array().length; i++) {
+        console.log(categoryChannels.length);
+        for (i = 0; i < categoryChannels.length; i++) {
             console.log('run1');
             if (categoryChannels[i].members.array()[0] == undefined) {
                 console.log('run2');
