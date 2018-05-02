84d55ee3e156f5da7cd87cf5615642de51d614e6
warhammercasey <warhammercasey@gmail.com>
2018-05-01 21:23:29 -0600

    t

--- bot.js
+++ bot.js
@@ -50,7 +50,7 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
             while (userChannel.parent == null) {
                 var start = new Date().getTime();
                 for (var i = 0; i < 1e7; i++) {
-                    if ((new Date().getTime() - start) > milliseconds) {
+                    if ((new Date().getTime() - start) > 1) {
                         break;
                     }
                 }
