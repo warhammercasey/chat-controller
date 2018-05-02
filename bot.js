26a5043267a4d73a141fe150f0a6dd9934f8d7e3
warhammercasey <warhammercasey@gmail.com>
2018-05-01 21:03:19 -0600

    t

--- bot.js
+++ bot.js
@@ -49,15 +49,12 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
             userChannel = newMember.guild.channels.find('name', categoryChannels[0].name.substring(0, categoryChannels[0].name.lastIndexOf(" ")) + ' ' + (parseInt(categoryChannels[0].name.split(" ").pop()) + 1).toString());
             categoryChannels = userChannel.parent.children.array();
         }
-        console.log(categoryChannels);
         var emptyChannels = [];
-        console.log(categoryChannels.length);
         for (i = 0; i < categoryChannels.length; i++) {
             if (categoryChannels[i].members.array()[0] == undefined) {
                 emptyChannels.push(categoryChannels[i]);
             }
         }
-        console.log(emptyChannels.length);
         if (emptyChannels.length == 0) {
             var permissions = categoryChannels[0].permissionOverwrites.array();
             categoryChannels[categoryChannels.length - 1].clone(categoryChannels[categoryChannels.length - 1].name.substring(0, categoryChannels[categoryChannels.length - 1].name.lastIndexOf(" ")) + ' ' + (parseInt(categoryChannels[categoryChannels.length - 1].name.split(" ").pop()) + 1).toString()).then(clone => {
@@ -69,23 +66,23 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
             });
         }
     } else if (oldMember.voiceChannel != null && newMember.voiceChannel == null) {
-        /*categoryChannels = oldMember.voiceChannel.parent.children.array();
+        var categoryChannels = oldMember.voiceChannel.parent.children.array();
         var emptyChannels = [];
         for (i = 0; i < categoryChannels.length; i++) {
             if (categoryChannels[i].members.array()[0] == undefined) {
                 emptyChannels.push(categoryChannels[i]);
             }
         }
+        console.log(categoryChannels);
         if (emptyChannels.length > 1) {
-            for (i = 0; i < emptyChannels.length - 1; i++) {
-                emptyChannels[emptyChannels.length - i - 1].delete();
-            }
-            for (i = 0; i < categoryChannels.length; i++) {
-                if (parseInt(categoryChannels[i].name.split().pop()) != i + 1) {
-                    categoryChannels[i].setName(categoryChannels[i].name.substring(categoryChannels[i].name.lastIndexOf(' ')) + ' ' + (parseInt(categoryChannels[i].name.split(" ").pop()) + 1).toString());
+            for (i = 1; i < emptyChannels.length; i++) {
+                if (emptyChannels[i].deletable) {
+                    emptyChannels[i].delete();
+                } else {
+                    console.error('Missing delete permissions');
                 }
             }
-        }*/
+        }
     } else {
 
     }
