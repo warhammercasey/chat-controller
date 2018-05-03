e59e04c5708932d2170b3d8f699eec1a1a4b1648
warhammercasey <warhammercasey@gmail.com>
2018-05-02 21:26:17 -0600

    t

--- bot.js
+++ bot.js
@@ -37,7 +37,7 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
         var userChannel;
         if (newMember.guild.channels.find('name', categoryChannels[0].name.substring(0, categoryChannels[0].name.lastIndexOf(" ")) + ' ' + (parseInt(categoryChannels[0].name.split(" ").pop()) + 1).toString()) == null) {
             var permissions = categoryChannels[0].permissionOverwrites.array();
-            categoryChannels[categoryChannels.length - 1].clone(categoryChannels[categoryChannels.length - 1].name.substring(0, categoryChannels[categoryChannels.length - 1].name.lastIndexOf(" ")) + ' ' + (parseInt(categoryChannels[categoryChannels.length - 1].name.split(" ").pop()) + 1).toString());/*.then(clone => {
+            categoryChannels[categoryChannels.length - 1].clone(categoryChannels[categoryChannels.length - 1].name.substring(0, categoryChannels[categoryChannels.length - 1].name.lastIndexOf(" ")) + ' ' + (parseInt(categoryChannels[categoryChannels.length - 1].name.split(" ").pop()) + 1).toString()).then(clone => console.log('Created channel: ' + clone.name));/*.then(clone => {
                 clone.setParent(categoryChannels[categoryChannels.length - 1].parent);
                 newMember.guild.channels.find('id', clone.id).setUserLimit(categoryChannels[categoryChannels.length - 1].userLimit);
                 for (i = 0; i < permissions.length; i++) {
