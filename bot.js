65a55b3dfd3503837d6d84353466fec84f424647
warhammercasey <warhammercasey@gmail.com>
2018-05-02 21:23:03 -0600

    t

--- bot.js
+++ bot.js
@@ -37,13 +37,19 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
         var userChannel;
         if (newMember.guild.channels.find('name', categoryChannels[0].name.substring(0, categoryChannels[0].name.lastIndexOf(" ")) + ' ' + (parseInt(categoryChannels[0].name.split(" ").pop()) + 1).toString()) == null) {
             var permissions = categoryChannels[0].permissionOverwrites.array();
-            categoryChannels[categoryChannels.length - 1].clone(categoryChannels[categoryChannels.length - 1].name.substring(0, categoryChannels[categoryChannels.length - 1].name.lastIndexOf(" ")) + ' ' + (parseInt(categoryChannels[categoryChannels.length - 1].name.split(" ").pop()) + 1).toString()).then(clone => {
+            categoryChannels[categoryChannels.length - 1].clone(categoryChannels[categoryChannels.length - 1].name.substring(0, categoryChannels[categoryChannels.length - 1].name.lastIndexOf(" ")) + ' ' + (parseInt(categoryChannels[categoryChannels.length - 1].name.split(" ").pop()) + 1).toString());/*.then(clone => {
                 clone.setParent(categoryChannels[categoryChannels.length - 1].parent);
                 newMember.guild.channels.find('id', clone.id).setUserLimit(categoryChannels[categoryChannels.length - 1].userLimit);
                 for (i = 0; i < permissions.length; i++) {
                     clone.overwritePermissions(permissions[i].id, permissions[i]);
                 }
-            });
+            });*/
+            var clone = newMember.guild.channels.find('name', categoryChannels[categoryChannels.length - 1].name.substring(0, categoryChannels[categoryChannels.length - 1].name.lastIndexOf(" ")) + ' ' + (parseInt(categoryChannels[categoryChannels.length - 1].name.split(" ").pop()) + 1).toString());
+            clone.setParent(categoryChannels[categoryChannels.length - 1].parent);
+            clone.setUserLimit(categoryChannels[categoryChannels.length - 1].parent);
+            for (i = 0; i < permissions.length; i++) {
+                clone.overwritePermissions(permissions[i].id, permissions[i]);
+            }
             return;
         } else if (newMember.voiceChannel.name.split(" ").pop() == '1') {
             userChannel = newMember.guild.channels.find('name', categoryChannels[0].name.substring(0, categoryChannels[0].name.lastIndexOf(" ")) + ' ' + '2');
@@ -59,13 +65,19 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
         }
         if (emptyChannels.length == 0) {
             var permissions = categoryChannels[0].permissionOverwrites.array();
-            categoryChannels[categoryChannels.length - 1].clone(categoryChannels[categoryChannels.length - 1].name.substring(0, categoryChannels[categoryChannels.length - 1].name.lastIndexOf(" ")) + ' ' + (parseInt(categoryChannels[categoryChannels.length - 1].name.split(" ").pop()) + 1).toString()).then(clone => {
+            categoryChannels[categoryChannels.length - 1].clone(categoryChannels[categoryChannels.length - 1].name.substring(0, categoryChannels[categoryChannels.length - 1].name.lastIndexOf(" ")) + ' ' + (parseInt(categoryChannels[categoryChannels.length - 1].name.split(" ").pop()) + 1).toString());/*.then(clone => {
                 clone.setParent(categoryChannels[categoryChannels.length - 1].parent);
                 clone.setUserLimit(categoryChannels[categoryChannels.length - 1].userLimit);
                 for (i = 0; i < permissions.length; i++) {
                     clone.overwritePermissions(permissions[i].id, permissions[i]);
                 }
-            });
+            });*/
+            var clone = newMember.guild.channels.find('name', categoryChannels[categoryChannels.length - 1].name.substring(0, categoryChannels[categoryChannels.length - 1].name.lastIndexOf(" ")) + ' ' + (parseInt(categoryChannels[categoryChannels.length - 1].name.split(" ").pop()) + 1).toString());
+            clone.setParent(categoryChannels[categoryChannels.length - 1].parent);
+            clone.setUserLimit(categoryChannels[categoryChannels.length - 1].userLimit);
+            for (i = 0; i < permissions.length; i++) {
+                clone.overwritePermissions(permissions[i].id, permissions[i]);
+            }
         }
     } else if (oldMember.voiceChannel != null && newMember.voiceChannel == null) {
         //var categoryChannels = []; //= oldMember.voiceChannel.parent.children.array();
